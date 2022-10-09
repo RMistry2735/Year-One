9cecc141133cc30f39de5dd6182b5138ff3c6ae2
RMistry2735 <114408503+RMistry2735@users.noreply.github.com>
2022-10-09 22:38:49 +0100

    Update Lab Three.py

--- Lab Three.py
+++ Lab Three.py
@@ -1,13 +1,25 @@
+#Question 1
 name = input("What is your name? ")
 if name == "":
     print("Hello, Stranger!")
 else:
     print("Hello", name,". Good to meet you")
     
-
-password = input("Please input a password: ")
-password2 = input("Please input the password again: ")
+#Question 2
+password = input("Please enter a password:")
+password2 = input("Please enter the password again:")
 if password == password2:
     print("Password set")
 else:
     print("The 2 passwords were different")
+
+#Question 3
+password = input("Please input a password:")
+validation = input("Please input the password again:")
+if password == validation:
+    print("Password set")
+else:
+    print("The 2 passwords were different")
+                  
+#Question 4
+ 
