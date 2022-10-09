436255ae2d45a78e33e2ed5d16455a695fa4d8c7
RMistry2735 <114408503+RMistry2735@users.noreply.github.com>
2022-10-09 22:58:11 +0100

    Update Lab Three.py

--- Lab Three.py
+++ Lab Three.py
@@ -14,12 +14,39 @@ else:
     print("The 2 passwords were different")
 
 #Question 3
-password = input("Please input a password:")
-validation = input("Please input the password again:")
-if password == validation:
+password = input("Please enter a password:")
+password2 = input("Please enter the password again:")
+if password == password2 and 8 <= len(password) <= 12:
     print("Password set")
 else:
-    print("The 2 passwords were different")
-                  
+    print("Password not accepted")
+    
 #Question 4
- 
+BAD_PASSWORDS = ['password', 'letmein', 'sesame', 'hello', 'justinbieber']
+password = input("Please enter a password:")
+password2 = input("Please enter the password again:")
+if password == password2 and 8 <= len(password) <= 12 and password not in BAD_PASSWORDS:
+    print("Password set")
+else:
+    print("Password not accepted")
+
+#Question 5
+def SetPassword():
+    password = input("Please enter a password:")
+    password2 = input("Please enter the password again:")
+    if password == password2 and 8 <= len(password) <= 12 and password not in BAD_PASSWORDS:
+        print("Password set")
+    elif len(password) < 8:
+        print("Password is too short, please try again")
+        SetPassword()
+    elif len(password) > 13:
+        print("Password is too long, please try again")
+        SetPassword()
+    elif password is in BAD_PASSWORDS:
+        print("Password not accepted, please try again")
+        SetPassword()
+    else:
+        print("Password not accepted, please try again")
+        SetPassword()
+
+SetPassword()
