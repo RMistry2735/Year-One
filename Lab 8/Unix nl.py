99248d753a28e50833fc8c3fd8d54079ab11cbbe
RMistry2735 <114408503+RMistry2735@users.noreply.github.com>
2022-11-14 14:46:50 +0000

    Create Unix nl.py

---
+++ Lab 8/Unix nl.py
@@ -0,0 +1,12 @@
+import sys
+
+def nl():
+    f = open(userinput)
+    for words in f:
+        n = 0
+        print(str(n) + words)
+        n +=1
+
+if __name__ = '__main__'
+    userinput = sym.argv 
+    
