e9b4e24abdc400950bfab4ae455233f12cadd73b
RMistry2735 <114408503+RMistry2735@users.noreply.github.com>
2022-12-11 16:56:36 +0000

    Update Task 2 uptd.py

--- Assignment/Task 2 uptd.py
+++ Assignment/Task 2 uptd.py
@@ -1,14 +1,13 @@
 import statistics
 
 runners = []
-runnersTime = []
-runnerCount = 0
-print("Enter the runners numbers and their times separated by two colons e.g.(123::300)")
-print("Type 'END' when you are done")
+runnerstime = []
+runnercnt = 0
+print("Enter the runners numbers and their times separated by two colons e.g.(123::300)\nType 'END' when you are done")
 while True:
     usr_in = input()
     temp = usr_in.split("::")
-    if usr_in == "END" and runnerCount == 0:
+    if usr_in == "END" and runnercnt == 0:
         print("No data found, nothing to do.")
         exit()
     if usr_in =="END":
@@ -17,30 +16,30 @@ while True:
         print("Error in data stream, Ignoring, Continue.")
     else:
         runners.append(int(temp[0]))
-        runnersTime.append(int(temp[1]))
-        runnerCount +=1
+        runnerstime.append(int(temp[1]))
+        runnercnt +=1
 
 print("Total runners:",len(runners))
 
-fastest = min(runnersTime)
-fastestmin = int(fastest/60)
-fastest -= (fastestmin*60)
-fastestsec = fastest
-print("Fastest Time:",fastestmin,"minutes,",fastestsec,"seconds")
+fst = min(runnerstime)
+fstmin = int(fst/60)
+fst -= (fstmin*60)
+fstsec = fst
+print("Fastest Time:",fstmin,"minutes,",fstsec,"seconds")
 
-slowest = max(runnersTime)
-slowestmin = int(slowest/60)
-slowest -= (slowestmin*60)
-slowestsec = slowest
-print("Slowest Time:",slowestmin,"minutes,",slowestsec,"seconds")
+slst = max(runnerstime)
+slstmin = int(slst/60)
+slst -= (slstmin*60)
+slstsec = slst
+print("Slowest Time:",slstmin,"minutes,",slstsec,"seconds")
 
-avg = statistics.mean(runnersTime)
+avg = statistics.mean(runnerstime)
 avgmin = int(avg/60)
 avg -= (avgmin*60)
 avgsec = int(avg)
 print("Average Time:",avgmin,"minutes,",avgsec,"seconds")
 
-new_list = list(map(list, zip(runners, runnersTime)))
+new_list = list(map(list, zip(runners, runnerstime)))
 sorted_list = sorted(new_list, key=lambda x: x[1], reverse=False)
-fastest = sorted_list[0][0]
-print(f"The fastest runner was #{fastest}")
+fst = sorted_list[0][0]
+print(f"The fastest runner was #{fst}")
